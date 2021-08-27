fcabd9bb20907cdc53388562dea65117eb23bc77
ITALOTORIBIO <italo_alexander_441@hotmail.com>
2021-08-26 22:44:32 -0500

    Mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proyecto_SmartCampus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
