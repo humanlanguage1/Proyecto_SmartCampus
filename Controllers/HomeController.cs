db9f75bfe0fe92e7125fd280562de6cb330e4b1f
ITALOTORIBIO <italo_alexander_441@hotmail.com>
2021-09-02 16:24:15 -0500

    MainPage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Proyecto_SmartCampus.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
