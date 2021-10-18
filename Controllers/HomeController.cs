33ea2d6b8de58b19e4fe5b8080ca9e0aa3a492b8
ITALOTORIBIO <italo_alexander_441@hotmail.com>
2021-10-18 14:56:59 -0500

    proyecto1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Proyecto_SmartCampus.Models;
+using System.Net.Http;
+using System.Text;
+using System.Web;
 
 namespace Proyecto_SmartCampus.Controllers
 {
@@ -21,6 +24,52 @@ namespace Proyecto_SmartCampus.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<String> sugerenciasDb(string sug)
+        {
+            using (var client = new HttpClient())
+            {
+                var jsonData = System.Text.Json.JsonSerializer.Serialize(new
+                    {
+                        sugerencia=sug
+                    });
+                var content = new StringContent(jsonData);
+
+                content.Headers.ContentType.CharSet= string.Empty;
+                content.Headers.ContentType.MediaType = "application/json";
+
+                var response = await client.PostAsync("https://prod-16.westus.logic.azure.com:443/workflows/99a2a438fbc644febdc5570fc14ca57b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=si0j9WOtp3FvdCkiQ50bH6pBbLup9RZ132UCtYqtW4g",content);
+
+                
+
+                var respuesta = "Sugerencia enviada";
+                return respuesta;
+            }
+        }
+
+        public async Task<String> contactar(string apell, string celu, string correo, string mensj, string nomb)
+        {
+            using (var client = new HttpClient())
+            {
+                var jsonData = System.Text.Json.JsonSerializer.Serialize(new
+                    {
+                        apellidos=apell,
+                        celular=celu,
+                        email=correo,
+                        mensaje=mensj,
+                        nombres=nomb
+                    });
+                var content = new StringContent(jsonData);
+
+                content.Headers.ContentType.CharSet= string.Empty;
+                content.Headers.ContentType.MediaType = "application/json";
+
+                var response = await client.PostAsync("https://prod-100.westus.logic.azure.com:443/workflows/e86a9178840246ce8b5fcbf82e5f12e0/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=GpPDGBq1wa8ZsGPy1KwJywPhh2KgxRqNPV4zMiYyNBw",content);
+
+                                
+                var respuesta = "Nos pondremos en contacto contigo";
+                return respuesta;
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
