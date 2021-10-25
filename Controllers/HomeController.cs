31a1d4e0b3295b7d6524393b3d5e89f0ae4adb64
ITALOTORIBIO <italo_alexander_441@hotmail.com>
2021-10-25 17:36:53 -0500

    Contactodb

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,7 +38,7 @@ namespace Proyecto_SmartCampus.Controllers
                 content.Headers.ContentType.CharSet= string.Empty;
                 content.Headers.ContentType.MediaType = "application/json";
 
-                var response = await client.PostAsync("https://prod-16.westus.logic.azure.com:443/workflows/99a2a438fbc644febdc5570fc14ca57b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=si0j9WOtp3FvdCkiQ50bH6pBbLup9RZ132UCtYqtW4g",content);
+                var response = await client.PostAsync("https://prod-143.westus.logic.azure.com:443/workflows/b376c38dbbb54a1aa348276ef7fdbf1b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=wcDWgIaCxx4vZy_C2NmV3nY6MUEUa1lO9YJ4HUVOZ60",content);
 
                 
 
@@ -64,7 +64,7 @@ namespace Proyecto_SmartCampus.Controllers
                 content.Headers.ContentType.CharSet= string.Empty;
                 content.Headers.ContentType.MediaType = "application/json";
 
-                var response = await client.PostAsync("https://prod-100.westus.logic.azure.com:443/workflows/e86a9178840246ce8b5fcbf82e5f12e0/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=GpPDGBq1wa8ZsGPy1KwJywPhh2KgxRqNPV4zMiYyNBw",content);
+                var response = await client.PostAsync("https://prod-118.westus.logic.azure.com:443/workflows/955e202485134908adace0f785f84f0f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=C_FDgUOy8xVKBrGRW0YWFl0F9lE7Qus80lbUXo91LPw",content);
 
                                 
                 var respuesta = "Nos pondremos en contacto contigo";
